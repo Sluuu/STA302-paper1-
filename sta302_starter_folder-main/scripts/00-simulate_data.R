c433e6bc280db2c7e1e7aa5d48c79134c63872ce
Sean Liu <yuhsiang.liu@mail.utoronto.ca>
2024-01-24 15:49:18 +0000

    Upload raw data, editing paper

--- sta302_starter_folder-main/scripts/00-simulate_data.R
+++ sta302_starter_folder-main/scripts/00-simulate_data.R
@@ -10,6 +10,18 @@
 
 #### Workspace setup ####
 library(tidyverse)
+library(opendatatoronto)
+library(dplyr)
+library(readxl)
+download.file(url = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/52182614-1f0b-42be-aca4-3f86dc8e004c/resource/5cf04d72-dd7b-499c-a28a-e8aceb44f249/download/2022%20Income%20Scenario%20-%20Without%20Subsidies,%20Market%20Rent.csv", 
+              destfile = "raw_data.csv")
+
+
+data <- read.csv("raw_data.csv")
+
+
+# get package
+
 
 # [...UPDATE THIS...]
 
@@ -18,7 +30,6 @@ data_with_sub <- read.csv("/cloud/project/sta302_starter_folder-main/inputs/data
 data_avg_rent_nosub <- read.csv("/cloud/project/sta302_starter_folder-main/inputs/data/2022 Income Scenario - Without Subsidies, Average Rent.csv")
 data_mkt_rent_nosub <- read.csv("/cloud/project/sta302_starter_folder-main/inputs/data/2022 Income Scenario - Without Subsidies, Market Rent.csv")
 
-
-
+x <- data_mkt_rent_nosub
 
 
